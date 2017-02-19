93698f981bec546b99a5c5d4db7ee23f0b86b8c1
JonathanFu <fujianglai@gmail.com>
2017-02-19 14:02:04 +1100

    update

--- README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/CSharpMark/pen/ZYXYXd.
-
- Search Flickr RSS feed for recently added images using AngularJS. AngularJS uses the JSON data to dynamically build the HTML to display the images.
--- js/index.js
+++ js/index.js
@@ -1,64 +1,74 @@
 (function (angular) {
-  'use strict';
-  angular.module('formFlickr', [])
-    .controller('FlickrController', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+    'use strict';
+    angular.module('searchFlickr', ['ui.materialize'])
+        .controller('FlickrController', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
 
-      $scope.searching = false;
-      $scope.message = '';
-      $scope.images = {};
-
-      $scope.search = function (searchCriteria) {
-
-        if (!searchCriteria.tags) {
-          return false;
-        }
-
-        $scope.searching = true;
-        $scope.message = '';
-        $scope.images = {};
-        $scope.form.tags.$setUntouched();
-        $scope.form.tags.$setValidity();
-
-        // build URL for Flickr API
-        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne";
-        flickrAPI = flickrAPI + "?jsoncallback=JSON_CALLBACK"
-          + "&tags=" + encodeURIComponent($scope.searchCriteria.tags)
-          + "&format=json";
-
-        $http.jsonp(flickrAPI)
-          .success(function (data, status, headers, config) {
             $scope.searching = false;
-            $scope.imagesStatus = status;
-            if(!Array.isArray(data.items) || data.items.length === 0){
-              $scope.message = 'No Records found';
-              return false;
-            }
-          $scope.images = data.items.map(function (item) {
-            item.author = item.author.replace('nobody@flickr.com ("', '').replace('")','');
-            var tagArr = item.tags.split(' ');
-            var tagHtml = '';
-            tagArr.forEach(function (tag) {
-              tagHtml += '<span class="tag">' + tag + '</span><span>&nbsp;</span>';
-            });
-            item.tagList = tagHtml;
-            return item;
-          });
+            $scope.message = '';
+            $scope.images = {};
 
-        }).error(function (data, status, headers, config) {
-          $scope.images = data;
-          $scope.searching = status;
-          $scope.message = data;
-        });
+            $scope.search = function () {
+                searchHandler();
+            };
 
-      };
+            $scope.isSearching = function () {
+                return !!$scope.searching;
+            };
 
-      $scope.isSearching = function () {
-        return !!$scope.searching;
-      };
+            $scope.renderHtml = function (htmlCode) {
+                return $sce.trustAsHtml(htmlCode);
+            };
 
-      $scope.renderHtml = function (htmlCode) {
-        return $sce.trustAsHtml(htmlCode);
-      };
+            $scope.searchTag = function (obj, event) {
+                $scope.tags = event.target.dataset.tag;
+                searchHandler();
+            };
 
-    }]);
+
+            var searchHandler = function () {
+
+                if (!$scope.tags) {
+                    return false;
+                }
+
+                $scope.searching = true;
+                $scope.message = '';
+                $scope.images = {};
+                $scope.form.tags.$setUntouched();
+                $scope.form.tags.$setValidity();
+
+                var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne";
+                var reqUrl = flickrAPI + "?jsoncallback=JSON_CALLBACK&format=json"
+                    + "&tags=" + encodeURIComponent($scope.tags);
+
+                $http.jsonp(reqUrl)
+                    .success(function (data, status, headers, config) {
+                        $scope.searching = false;
+                        $scope.imagesStatus = status;
+                        if (!Array.isArray(data.items) || data.items.length === 0) {
+                            $scope.message = 'No Records found';
+                            return false;
+                        }
+                        $scope.images = data.items.map(function (item) {
+                            item.author = item.author.replace('nobody@flickr.com ("', '').replace('")', '');
+                            var tagArr = item.tags.split(' ');
+                            var tagHtml = '<ul>';
+                            tagArr.forEach(function (tag) {
+                                if (!tag || tag.length <= 1) return;
+                                tagHtml += '<li><span class="tag" data-tag="'+tag+'">' + tag + '</span></li>';
+                            });
+                            item.tagList = tagHtml + '</ul>';
+                            return item;
+                        });
+
+                    }).error(function (data, status, headers, config) {
+                    $scope.images = data;
+                    $scope.searching = status;
+                    $scope.message = data;
+                });
+
+            };
+
+
+        }]);
 })(window.angular);
